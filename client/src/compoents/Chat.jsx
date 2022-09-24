bb9503baf33afc64329ec2c81ab74e3c7ffbc6f5
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-24 15:55:18 +0530

    added useLocation hook to get search query

--- client/src/compoents/Chat.jsx
+++ client/src/compoents/Chat.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
-
-export default function Chat() {
+import { useEffect } from 'react'
+import queryString from "query-string"
+import { useLocation } from 'react-router-dom'
+export default function Chat({location}) {
+    const {search} =useLocation();
+    const {name,room} = queryString.parse(search);
+    console.log(name,room);
+    useEffect(()=>{
+   
+        
+    },[])
   return (
     <div>Chat</div>
   )
