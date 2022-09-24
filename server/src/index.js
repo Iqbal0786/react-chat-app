46fed784ec70e4848a2bc96e957ad5a204caabe0
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-24 16:13:43 +0530

    added cors middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,12 @@ const express= require("express");
 const socketio= require("socket.io");
 const http= require("http")
 const app = express();
-const router= require("./routes/router")
+const router= require("./routes/router");
+const cors= require("cors");
 const  server= http.createServer(app); // createing server
 const io= socketio(server);
 app.use(router)
-
+app.use(cors())
 io.on("connection",(socket)=>{
     console.log("A new user is connected!!");
     socket.on("disconnect" ,()=>{
