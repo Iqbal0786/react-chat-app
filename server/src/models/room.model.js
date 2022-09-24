de8cb379145d59c26a9c1ddbb01c08e57d9e2550
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-24 20:58:02 +0530

    Update room.model.js

--- server/src/models/room.model.js
+++ server/src/models/room.model.js
@@ -1,9 +1,12 @@
 const mongoose= require("mongoose");
 
 const roomSchema= new mongoose.Schema({
-       userName:[{type:String,required:true}],
+       user_id:{type:String,required:true},
+       userName:{type:String,required:true},
        roomName:{type:String,required:true}
 },{
     versionKey:false,
     timestamps:true
 })
+
+module.exports= mongoose.model("rooms",roomSchema)
