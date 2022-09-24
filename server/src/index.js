88914e92ef427957d7a86d332e65132ed54fb67a
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-24 14:28:25 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,8 +4,15 @@ const http= require("http")
 const app = express();
 const router= require("./routes/router")
 const  server= http.createServer(app); // createing server
-
+const io= socketio(server);
 app.use(router)
+
+io.on("connection",(socket)=>{
+    console.log("A new user is connected!!");
+    socket.on("disconnect" ,()=>{
+        console.log("User had left !!");
+    })
+})
 
 server.listen(process.env.PORT || 5000, ()=>{
     console.log("server is live on port 5000");
