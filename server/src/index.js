fa84db23744acb8e31c16b00799cf1c75c48ffdb
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-24 14:22:09 +0530

    added router

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,10 @@ const express= require("express");
 const socketio= require("socket.io");
 const http= require("http")
 const app = express();
+const router= require("./routes/router")
 const  server= http.createServer(app); // createing server
+
+app.use(router)
 
 server.listen(process.env.PORT || 5000, ()=>{
     console.log("server is live on port 5000");
