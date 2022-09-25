e60eb9643076b2d52922077055d2ac73bb6c90b6
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-25 23:08:53 +0530

    added message routes

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -3,6 +3,7 @@ const express= require("express");
 const router= express.Router();
 const Room= require("../models/room.model");
 const Chat= require("../models/chat.model")
+const Message=require("../models/message.model")
 
 router.get("/" , (req,res)=>{
     res.send("server is live now")
@@ -57,6 +58,32 @@ router.post("/chats" , async (req,res)=>{
 
 
 // chat routes end
+
+
+// message route start 
+
+router.get("/message" , async (req,res)=>{
+    try {
+        const messages= await Message.find().populate({path:"chat_id", select:["userName","roomName","user_id"]}).lean().exec();
+        return res.status(200).send(messages)
+    } catch (error) {
+        res.status(400).send({message:error.message})
+    }
+    
+})
+router.post("/message" , async (req,res)=>{
+    try {
+        
+        const message= await Message.create(req.body);
+        return res.status(201).send(message)
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).send({message:error.message})
+    }
+    
+})
+
+// message route ends
 
 // router.delete("/rooms" , async (req,res)=>{
 //     try {
