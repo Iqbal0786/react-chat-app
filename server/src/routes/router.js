b3db640b20b12fa358f2f6d15d46029d7dbfabcd
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-25 16:37:03 +0530

    added get route for room model

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,11 +1,22 @@
 
 const express= require("express");
 const router= express.Router();
+const Room= require("../models/room.model")
 
 router.get("/" , (req,res)=>{
     res.send("server is live now")
 })
 
+
+router.get("/rooms" , async (req,res)=>{
+    try {
+        const rooms= await Room.find().lean().exec();
+        return res.status(200).send(rooms)
+    } catch (error) {
+        res.status(400).send({message:error.message})
+    }
+    
+})
 
 
 
