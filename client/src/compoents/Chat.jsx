c70e7b73e63697fe4d83c02463bb53c44b4da896
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-26 00:22:35 +0530

    Update Chat.jsx

--- client/src/compoents/Chat.jsx
+++ client/src/compoents/Chat.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import queryString from "query-string"
 import { useLocation } from 'react-router-dom'
+import {Stack , Container ,Form, Button} from "react-bootstrap"
 import io from "socket.io-client";
 let socket;
 export default function Chat() {
@@ -24,6 +25,31 @@ export default function Chat() {
     },[ENDPOINT,search]);
 
   return (
-    <div>Chat</div>
+    <Container className='mt-5'>
+
+<Stack gap={3}>
+    <div className="bg-light border" style={{textAlign:"center"}}>Welcome to  CodingMeme </div>
+    <Form.Floating className="mb-3">
+        <Form.Control
+          id="floatingInputCustom"
+          type="text"
+          placeholder="messages"
+          disabled
+          value={"hii am iqbal"}
+          style={{height:"400px"}}
+        />
+      </Form.Floating>
+      <Stack direction="horizontal" gap={3}>
+      <Form.Control
+          id="floatingInputCustom"
+          type="text"
+          placeholder="type messages....."
+        />
+        <Button>
+          Send
+        </Button>
+    </Stack>
+  </Stack>
+    </Container>
   )
 }
