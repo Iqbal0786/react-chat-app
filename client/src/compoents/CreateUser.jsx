639dc42f7f0e3d2531487054e9c3497303a5a818
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-27 11:21:36 +0530

    added form

--- client/src/compoents/CreateUser.jsx
+++ client/src/compoents/CreateUser.jsx
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import { Modal, Form } from "react-bootstrap";
 
- export default function CreateUser({show,modalCloseHanlder}) {
-//   const [show, setShow] = useState(false);
-  const handleClose = () => {modalCloseHanlder()}
-
+export default function CreateUser({ show, modalCloseHanlder }) {
+  //   const [show, setShow] = useState(false);
+  const handleClose = () => {
+    modalCloseHanlder();
+  };
 
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Create Account </Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          {" "}
+          <Form style={{ width: "70%", marginLeft: "50px", marginTop: "10px" }}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Your Name </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your name ..."
+                onChange={(e) => {}}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Room</Form.Label>
+                <Form.Select>
+                    <option value="">CodingMeme</option>
+                    <option value="">CodingMeme</option>
+                    <option value="">CodingMeme</option>
+                </Form.Select>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
