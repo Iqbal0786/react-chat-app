a5239d67e6cfb0e61cf72aff2a911188e425aff4
Mohammad Iqbal <iqbal240456@gmail.com>
2022-09-29 13:58:29 +0530

    added room select option in join component

--- client/src/compoents/Join.jsx
+++ client/src/compoents/Join.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import {Button, Card ,Form} from "react-bootstrap"
 import { Link } from 'react-router-dom';
+import { io } from 'socket.io-client';
 import CreateUser from './CreateUser';
-
+let socket;
 export default function Join() {
+  const ENDPOINT="https://react-chat-app-db.herokuapp.com/";
     const [name,setName]=useState('');
     const [room,setRoom]=useState("");
+    const [rooms,setRooms]=useState("");
     const [show,setShow]=useState(false);
     const modalHandler = ()=>{
         setShow(true)
@@ -14,6 +17,24 @@ export default function Join() {
     const modalCloseHanlder=()=>{
        setShow(false)
     }
+
+    useEffect(()=>{
+      socket=io(ENDPOINT);
+      socket.on("getRooms",(roomData)=>{
+           //  console.log(rooms)
+           setRooms([...roomData])
+      })
+    console.log(socket);
+    
+   
+      return () => {
+       // socket.emit("disconnect");
+       socket.off();
+     };
+   
+    },[
+       
+    ])
   return (
     <>
     <Card  style={{width:"500px" , height:"300px" , margin:"auto", marginTop:"5%"}}>
@@ -26,11 +47,23 @@ export default function Join() {
         }} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Room</Form.Label>
-        <Form.Control type="email" placeholder="Enter room name to join ..."  onChange={(e)=>{
-            setRoom(e.target.value)
-        }}/>
-      </Form.Group>
+              <Form.Label>Room</Form.Label>
+                <Form.Select
+                onChange={(e)=>{
+                  setRoom(e.target.value)
+                }}
+                   name='roomName'
+                >
+                    <option>Choose room</option>
+                  {
+                    rooms.length? rooms.map((e)=>{
+                        return (
+                           <option key={e._id} >{e.roomTitle}</option>
+                        )
+                    }): <option>choose</option>
+                  }
+                </Form.Select>
+            </Form.Group>
       <Link onClick={(e)=>!name || !room?e.preventDefault():null} to={`/chat?name=${name}&room=${room}`}>
       <Button type='submit'>Join Room </Button>
       </Link>
